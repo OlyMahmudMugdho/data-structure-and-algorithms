19e48e18ca6f15c4a1da722cb1bda1e566bba235
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-06-24 21:51:16 +0600

    added Main

--- searching-algorithms/Main.java
+++ searching-algorithms/Main.java
@@ -1,1 +1,22 @@
 
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,9,2,4,8};
+        int[] sortedNums = {1,3,5,7,9};
+        int[] sortedNums2 = {9,7,5,3,1};
+        int res;
+
+        LinearSearch ls = new LinearSearch();
+        System.out.println("Linear Search : ");
+        System.out.println(ls.search(nums,2));
+
+        BinarySearch bs = new BinarySearch();
+        System.out.println("Binary Search (Descending) : ");
+        System.out.println(bs.ascendingSearch(sortedNums,9));
+
+        res = bs.descendingSearch(sortedNums2,5);
+        System.out.println("Binary Search (Descending) : ");
+        System.out.println(res);
+
+    }
+}
