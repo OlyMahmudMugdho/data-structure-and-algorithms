1add8c82b76678001855ac70ccd585b8a332534a
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-08-09 13:12:44 +0600

    Add files via upload

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,1 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
 
+        ll.push(5);
+        ll.push(12);
+        System.out.println(ll.get(1).value);
+    }
+}
