fbf27be8e210813c644568e635747ed7d3bc8826
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-01-26 20:34:38 +0600

    solved Tree Sum in java [DFS]

---
+++ BST/TreeSumDFS.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+public class TreeSumDFS {
+
+
+    /*
+     *           1
+     *         /    \
+     *        2      3
+     *       / \      \
+     *      4   5      6
+     *
+     * */
+
+    public static int sum(Node root) {
+        int sum = 0;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+
+        while (!stack.isEmpty()) {
+            Node current = stack.pop();
+            sum += current.value;
+
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node two = new Node(2);
+        Node three = new Node(3);
+        Node four = new Node(4);
+        Node five = new Node(5);
+        Node six = new Node(6);
+
+        root.left = two;
+        root.right = three;
+        two.left = four;
+        two.right = five;
+        three.right = six;
+
+        System.out.println(sum(root));
+    }
+}
