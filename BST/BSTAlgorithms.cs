e68712a7d44245f2f5ba27200cffb42197b80945
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-01-29 18:19:40 +0600

    added Depth First Value algorithm in C#

---
+++ BST/BSTAlgorithms.cs
@@ -0,0 +1,72 @@
+public class BSTAlgorithms
+{
+    public static void DepthFirstValue(Node root)
+    {
+        Stack<Node> stack = new Stack<Node>();
+        stack.Push(root);
+
+        while (stack.Count != 0)
+        {
+            Node current = stack.Pop();
+            
+            Console.Write(current.value + " ");
+
+            if (current.right != null)
+            {
+                stack.Push(current.right);
+            }
+
+            if (current.left != null)
+            {
+                stack.Push(current.left);
+            }
+        }
+    }
+
+    
+    /*
+     *            7
+     *         /     \
+     *        5      10
+     *      /  \    /  \
+     *     1    6  8   12
+     * 
+     *
+     */
+    public static void Main(string[] args)
+    {
+        Node root = new Node(7);
+        Node five = new Node(5);
+        Node ten = new Node(10);
+        Node one = new Node(1);
+        Node six = new Node(6);
+        Node eight = new Node(8);
+        Node twelve = new Node(12);
+
+        root.left = five;
+        root.right = ten;
+        five.left = one;
+        five.right = six;
+        ten.left = eight;
+        ten.right = twelve;
+        
+        DepthFirstValue(root);
+        
+    }
+}
+
+public class Node
+{
+    public int value;
+    public Node left;
+    public Node right;
+    
+    public Node() {}
+
+    public Node(int value)
+    {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
