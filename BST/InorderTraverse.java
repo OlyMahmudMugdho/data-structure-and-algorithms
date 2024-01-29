d8860622bb65ff2da3f5501e72428044cc4ce54b
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-01-29 19:06:14 +0600

    BST in order traversal using java

---
+++ BST/InorderTraverse.java
@@ -0,0 +1,23 @@
+public class InorderTraverse {
+
+    public static void traverse(Node root){
+        if (root == null) {
+            return;
+        }
+        traverse(root.left);
+        System.out.println(root.value);
+        traverse(root.right);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(100);
+        root.left = new Node(20);
+        root.right = new Node(200);
+        root.left.left = new Node(10);
+        root.left.right = new Node(30);
+        root.right.left = new Node(150);
+        root.right.right = new Node(300);
+
+        traverse(root);
+    }
+}
