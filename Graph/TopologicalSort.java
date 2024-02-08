35ca1946072a658c8229d77c19506a01a26ea2b1
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-02-08 22:31:31 +0600

    getting inorder set

--- Graph/TopologicalSort.java
+++ Graph/TopologicalSort.java
@@ -1,7 +1,7 @@
-import java.util.List;
+import java.util.*;
 
 public class TopologicalSort {
-    public static int inOrder(Graph graph, char vertex) {
+    public static int getInOrder(Graph graph, char vertex) {
         int order = 0;
         List<Character> keys = graph.adjacencyList.keySet().stream().toList();
         for (char key : keys) {
@@ -11,13 +11,30 @@ public class TopologicalSort {
         }
         return order;
     }
+
+    public static Map<Character,Integer> inOrders(Graph graph) {
+        Map<Character,Integer> orders = new HashMap<>();
+        int count = 0;
+        Set<Character> keySet = graph.adjacencyList.keySet();
+        Set<Character> set = new TreeSet<>();
+
+        set.addAll(keySet);
+
+        for (char item : keySet) {
+            set.addAll(graph.adjacencyList.get(item));
+        }
+
+        for (char item : set) {
+            orders.put(item,getInOrder(graph,item));
+        }
+        return orders;
+    }
     public static void main(String[] args) {
         Graph graph = new Graph();
         graph.addEdge('A','B');
         graph.addEdge('A','D');
-        graph.addEdge('E','B');
         graph.addEdge('B','C');
 
-        System.out.println(inOrder(graph,'B'));
+        System.out.println(inOrders(graph).toString());
     }
 }
