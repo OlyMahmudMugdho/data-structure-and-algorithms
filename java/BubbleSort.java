9e98f1639a6ddce27d0546c16bbace7c71a67009
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-07 12:51:06 +0600

    bubble sorting in java

---
+++ java/BubbleSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class BubbleSort {
+
+    public static void sort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < (n - 1); i++) {
+            for (int j = 1; j < (n - i - 1); j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+    }
+
+    static void printArray(int[] array) {
+        for (var item : array) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {4,6,3,9,5,6,8};
+        printArray(array);
+        sort(array);
+        printArray(array);
+    }
+}
